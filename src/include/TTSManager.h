0540091e76ede07d6b971d502d81fcd6bb3e1ccb
pooja.jumana <pooja.jumanal@lge.com>
2019-11-11 17:04:46 +0530

    Fix coverity issue
    :Release Notes:
    Fix the following coverity issue
    - CID: 284745 Missing copy constructor (com_webos_service_tts)
    
    :Detailed Notes:
    - A single instance of TTSManager exist throughout process,so deleted
      the definition of copy constructor and copy-assignment operator.
    
    :Testing Performed:
    fixed and built on official build 1840
    
    :QA Notes:
    N/A
    
    :Issues Addressed:
    [PLAT-93138] 284745 Missing copy constructor (com_webos_service_tts)
    
    Change-Id: I175db71e3f4cf4f5ce4eb5c0d4fd6a1a0d4582f9

--- src/include/TTSManager.h
+++ src/include/TTSManager.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2018 LG Electronics, Inc.
+// Copyright (c) 2019 LG Electronics, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -29,6 +29,8 @@ class TTSManager
 public:
     TTSManager();
     virtual ~TTSManager();
+    TTSManager(const TTSManager&) = delete;
+    TTSManager& operator=(const TTSManager&) = delete;
     bool init(GMainLoop *mainLoop);
     void deInit();
 
